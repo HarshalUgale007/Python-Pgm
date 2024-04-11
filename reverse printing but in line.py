82694881f9e1f281a9450fece06251c676c8e9e4
Harshal Ugale <156573557+HarshalUgale007@users.noreply.github.com>
2024-04-11 09:49:50 +0530

    Add files via upload

---
+++ reverse printing but in line.py
@@ -0,0 +1,8 @@
+# ! $ % _ ():  "" ? *  <  >  % ^ & @  {} :  ()
+
+num = "Hello my name is Harsh and im learning a web developement "
+mun = ''
+for i in range(len(num)-1,-1,-1):
+  mun = mun + num[i]
+  
+print(mun)
